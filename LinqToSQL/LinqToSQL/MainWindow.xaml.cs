95765cddd59287787a255ac384036de5f080f294
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-28 09:52:29 -0700

    2nd commit

--- LinqToSQL/LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+
 
 namespace LinqToSQL
 {
@@ -20,9 +22,28 @@ namespace LinqToSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        LinqToSQLDataClassesDataContext dataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.UdemyClassDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+
+            InsertUniversites();
         }
+    public void InsertUniversites()
+        {
+            University yale = new University();
+            yale.Name = "Yale";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+        
     }
 }
